1a8f7677aea7368cc24aac9d8864de93143b762b
Mcheaven <115719615+FransManlangit@users.noreply.github.com>
2025-01-02 11:09:19 +0800

    deploy the backend

--- assets/common/baseUrl.js
+++ assets/common/baseUrl.js
@@ -3,8 +3,8 @@ import { Platform } from 'react-native'
 let baseURL = '';
 
 {Platform.OS == 'android'
-? baseURL = 'http://192.168.100.206:4000/api/v1/'
-: baseURL = 'http://localhost:4000/api/v1/'
+? baseURL = 'https://store-be-w6me.onrender.com/api/v1/'
+: baseURL = 'https://store-be-w6me.onrender.com/api/v1/'
 }
 
 export default baseURL;
